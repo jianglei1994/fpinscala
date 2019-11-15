e7501157c5f633e8fe291b7079d25237a1f1e1fc
jianglei43 <jianglei56@jd.com>
2019-11-15 16:36:29 +0800

    update:20191115

--- ch5/Stream.scala
+++ ch5/Stream.scala
@@ -84,9 +84,44 @@ object Stream{
 	def apply[A](as: A*): Stream[A] = 
 		if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 	
-	def test1(nz :  => Int) : () => Int = {
-		lazy val lazyNz = nz
-		() => lazyNz + 1;
+	/*5.8*/
+	def constant[A](a:A):Stream[A] = 
+		cons(a, constant(a))
+	
+	/*5.9*/
+	def from(n: Int): Stream[Int] = 
+		cons(n, from(n+1))
+	
+	/*5.10*/
+	def fibs: Stream[Int] = {
+		def go(f0: Int, f1: Int):Stream[Int] = {
+			cons(f0, go(f1, f0+f1))
+		}
+		go(0,1)
 	}
+	
+	/*ex5.11*/
+	def unfold[A,S](z: S)(f: S=>Option[(A,S)]): Stream[A] = {
+		f(z) match{
+			case Some((a,s)) => cons(a, unfold(s)(f))
+			case None => empty
+		}
+	}
+	
+	/*ex5.12*/
+	def fromViaUnfold(n: Int): Stream[Int] = {
+		unfold(n)( n => Some(n, n+1) )
+	}
+	
+	def fibsViaUnfold: Stream[Int] = {
+		unfold((0,1)){case (f0:Int,f1:Int) => Some(f0, (f1,f0+f1))}
+	}
+	
+	def constantViaUnfold[A](a: A): Stream[A] = {
+		unfold(a)( a => Some((a,a)))
+	}
+	
+	def mapViaUnfold[A,B](f: A=>B):Stream[]
+		
 
 }
